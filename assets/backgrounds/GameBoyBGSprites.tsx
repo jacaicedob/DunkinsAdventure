9bb9fc731aa6d91e1479c4aa45c35dacab79b936
Jaime Caicedo <jaime.caicedo@gmail.com>
2023-03-25 15:36:36 -0400

    Latest changes

---
+++ assets/backgrounds/GameBoyBGSprites.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.4" name="GameBoyBGSprites" tilewidth="8" tileheight="8" tilecount="468" columns="26">
+ <image source="../../../../../../Downloads/PixelPete'sArtAssets/PixelPete'sArtAssets/GameBoy/GameBoyBGSprites.png" width="208" height="144"/>
+</tileset>
---
+++ assets/backgrounds/gameboy_rpg_latest.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.4" name="gameboy_rpg_latest" tilewidth="8" tileheight="8" tilecount="2048" columns="64">
+ <image source="../../../../../../Downloads/gameboy_rpg_latest.png" width="512" height="256"/>
+</tileset>
